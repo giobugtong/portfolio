38e9fa613ff4d29ac30328f44551c54347bd73ae
Gio Bugtong <nlbugtong@gmail.com>
2021-10-01 03:36:14 +0800

    Start major redesign of Home page

---
+++ script.js
@@ -0,0 +1,22 @@
+const greeting = document.getElementById("greeting");
+
+const setGreeting = () => {
+    let day = new Date().getDay();
+    switch (day) {
+        case 0:
+            return "Sunday! The weekend is almost over!"
+        case 1:
+            return "Hope you're having a meaningful manic Monday"
+        case 2:
+            return "Tuesday: it's like Monday but better."
+        case 3:
+            return "Wednesday is Humpday!"
+        case 4:
+            return "Happy Thirst...I mean Thursday"
+        case 5:
+            return `"Salamat Hesus At Biyernes Ulit"`
+        case 6:
+            return "A Sober Saturday is a Sexy Saturday"
+    }
+}
+greeting.innerHTML = setGreeting();
